ea05125763545e14c92fa14e86baa450809be3e5
Windows 11 <shivershady@gmail.com>
2022-04-25 02:20:07 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import Home from "./home.js";
+import * as React from "react";
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import List from "./pages/todos/List";
+import Add from "./pages/todos/Add";
+import Edit from "./pages/todos/Edit";
+import GalleryImage from "./pages/todos/GalleryImage";
+import Login from "./pages/auth/Login";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <div className="container mx-auto">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<List/>}/>
+                        <Route path="/add" element={<Add/>}/>
+                        <Route path="/edit" element={<Edit/>}/>
+                        <Route path="/gallery" element={<GalleryImage/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
-export default App;
+export default App
